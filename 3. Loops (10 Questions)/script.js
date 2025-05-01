f52a6c6215f7c191140d594dd1986cba78858e72
suhailchishtiofficial <chishtisuhail9084@gmail.com>
2025-05-01 15:37:24 +0530

    Solve Questions

--- 3. Loops (10 Questions)/script.js
+++ 3. Loops (10 Questions)/script.js
@@ -1,42 +1,92 @@
 //21.	Write a for loop to print numbers from 1 to 50.
-
 // for(let i=1 ; i<=50 ; i++){
 //     console.log(i)
 // }
-//22.	Use a while loop to sum the numbers from 1 to 10.
-// while(){
 
+
+//22.	Use a while loop to sum the numbers from 1 to 10.
+// let a = 1;
+// let sum = 0;
+// while(a<11){
+//     sum =  sum + a;
+//     a++;
 // }
+// console.log(sum);
 
 //23.	Create a for...of loop to log each character of the string "JavaScript".
+// let str ="javascript";
+// for(let character of str){
+//     console.log(character);
+// }
 
 
 //24.	Write a for loop that skips even numbers between 1 and 20.
-// for(let i=1 ; i <=20 ; i++){
-//     console.log(i)
-//     i++;
+// for(let i=1 ; i <21 ; i++){
+//     if(i%2 === 0){}
+//     else{
+//         console.log(i);
+//     }
 // }
+//or
+// for(let i=1 ; i <21 ; i++){
+//     if(i%2 !== 0){
+//      console.log(i); 
+// }        
+// }
+
 
 //25.	Use a do...while loop to log numbers from 5 to 1.
 // let i=5
 // do {
 //     console.log(i);
 //     i--;
-// } while (i>0);
+// } 
+// while (i>0);
+
 
 //26.	Create a for loop that calculates the factorial of 5.
-// for (let i=1 ; i<6 ; i++){
-//     console.log(i)
-//     i*i
+// let fac = 1;
+// for(var i=5; i>0; i--){
+//     fac = fac*i;
 // }
+// console.log(fac);
+
 
 //27.	Write a nested loop to print a 3x3 grid of numbers.
-
+// var hold = 1;
+// for (var i=1; i<4; i++){
+//     var str ="";
+//     for(var j=1; j<4; j++){
+//         str += `${hold}     `;   
+//         hold++;
+//     }
+//     console.log(str);
+// }
 
 //28.	Use a for loop to reverse an array [1, 2, 3, 4].
+// var arr = [1,2,3,4,5];
+// for (var i=0; i < Math.floor(arr.length / 2); i++){
+//     var temp = arr[i];
+//     arr[i] = arr[arr.length - 1 - i];
+//     arr[arr.length - i - 1] = temp;
+// }
+// console.log(arr);
 
 
 //29.	Write a while loop that logs numbers from 1 to 100 divisible by 5.
-
+// let num = 1;
+// while(num<101){
+//     if(num%5 === 0) console.log(num);
+//     num++
+// }
 
 //30.	Use a for...in loop to iterate over an object and log its keys.
+// var obj = {
+//     name: "a",
+//     email:"a@gmail.com",
+//     age:26
+// }
+
+// for(var key in obj){
+//     console.log(key);
+// }
--- 4. Arrays (10 Questions)/script.js
+++ 4. Arrays (10 Questions)/script.js
@@ -1,39 +1,76 @@
 // 31.	Create an array of your top 5 favorite movies and log it.
 // let arr=["Johnwick","Spiderman","Ironman","Reacher","Needforspeed"];
-// console.log(arr);
+// arr.forEach(function(value){
+//     console.log(value);
+// })
+
 
 // 32.	Find and log the second element of an array.
 // let arr=["Johnwick","Spiderman","Ironman","Reacher","Needforspeed"];
 // console.log(arr[1]);
 
+
 // 33.	Add two new elements to the start of an array using .unshift().
-// let arr=["Johnwick","Spiderman","Ironman","Reacher","Needforspeed"];
-// let add=arr.unshift("Ghostrider","Dhamal");
-// console.log(arr);
+// var arr = [1,2,3,4,5]
+// arr.unshift(0);
+// arr.unshift(-1);
+
 
 // 34.	Remove the last element of an array and log the updated array.
 // let arr=["Johnwick","Spiderman","Ironman","Reacher","Needforspeed"];
 // let rem=arr.pop();
 // console.log(arr);
 
+
 // 35.	Use .slice() to extract the first three elements of an array.
 // let arr=["Johnwick","Spiderman","Ironman","Reacher","Needforspeed"];
-// let sl= arr.slice(3,0);
-// console.log(arr);
+// let sl= arr.slice(0,3);
+// console.log(sl);
 
+    
 // 36.	Find the index of a specific element in an array using .indexOf().
 // let arr=["Johnwick","Spiderman","Ironman","Reacher","Needforspeed"];
 // let index=arr.indexOf("Ironman");
 // console.log(index);
 
-// 37.	Check if a value exists in an array using .includes().
 
+// 37.	Check if a value exists in an array using .includes().
+// var arr = [1,2,3,4,5];
+// console.log(arr.includes(6));   
+// console.log(arr.includes(2)); 
 
 
 // 38.	Combine two arrays [1, 2] and [3, 4] using .concat().
+    // var arr1=[1,2];
+    // var arr2=[3,4];
+    // console.log(arr1.concat(arr2));
 
-
-// 39.	Sort an array of numbers [5, 2, 9, 1] in ascending order.
+// * 39.	Sort an array of numbers [5, 2, 9, 1] in ascending order.
+//     var arr = [5, 2, 9, 1];
+//     for(var j = 0; j<arr.length-1; j++){
+//         for(var i=0; i<arr.length -j -1; i++){
+//             if (arr[i] > arr[i+1]){
+//                 var temp = arr[i];
+//                 arr[i] =arr[i+1];
+//                 arr[i+1] = temp;
+//             }
+//         }
+//     }
+// console.log(arr);
 
 
 // 40.	Write a program that creates a copy of an array without mutating the original.
+// var arr= [11, 24, 36, 4, 5];
+// var arr2= [...arr];
+// console.log(arr2);
+// // or
+var arr= [11, 24, 36, 4, 5];
+var arr2= [];
+
+arr.forEach(function(value){
+    arr2.push(value);
+});
+
+arr2.pop();
+console.log(arr2);
+console.log(arr);
